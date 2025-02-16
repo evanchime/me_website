c25a1628362618152e79f93c2733be97292b2b71
Evan Chime <evanchime@yahoo.co.uk>
2025-02-16 19:41:27 +0000

    Document the accounts app's apps.py file

--- me_website_project/accounts/apps.py
+++ me_website_project/accounts/apps.py
@@ -1,6 +1,20 @@
+"""
+Django application configuration for the accounts app.
+
+Defines settings and metadata for the accounts application within a Django project.
+"""
+
 from django.apps import AppConfig
 
-
 class AccountsConfig(AppConfig):
+    """
+    Configuration class for the accounts application.
+    
+    Provides default settings and initialization for the Django accounts app.
+    """
+    
+    # Set default primary key field type for models
     default_auto_field = 'django.db.models.BigAutoField'
+    
+    # Name of the application as defined in the project's settings
     name = 'accounts'
