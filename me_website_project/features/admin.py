1898ad1a7934b07befef60458fdf36c1e352083e
Evan Chime <evanchime@yahoo.co.uk>
2025-02-17 21:31:26 +0000

    Document the features app's admin.py file

--- me_website_project/features/admin.py
+++ me_website_project/features/admin.py
@@ -1,9 +1,24 @@
-from django.contrib import admin
-from .models import Post
-from .models import Question
-from .models import Choice
+"""
+Django admin configuration for registering models with the 
+administration site.
 
-# Register your models here.
-admin.site.register(Post)
+This module imports relevant models from the current application and 
+registers them with the Django admin interface. Registration allows 
+authorized users to perform CRUD (Create, Read, Update, Delete) 
+operations on the models through the admin dashboard.
+
+Registered Models:
+    Post: Represents a blog post
+    Question: Represents a poll question object
+    Choice: Represents an answer choice associated with the poll 
+    Question.
+"""
+
+from django.contrib import admin
+from .models import Post, Question, Choice
+
+# Register models with the default ModelAdmin configurations
+# This enables basic admin functionality without customizations
+admin.site.register(Post)    
 admin.site.register(Question)
 admin.site.register(Choice)
