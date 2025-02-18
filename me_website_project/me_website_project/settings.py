6f8f53a08f47c0d221f02be57529b10d37994a4c
Evan Chime <evanchime@yahoo.co.uk>
2025-02-18 22:11:27 +0000

    Set DEBUG to True in dev and to False in production

--- me_website_project/me_website_project/settings.py
+++ me_website_project/me_website_project/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('ME_WEBSITE_DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
+
 
 ALLOWED_HOSTS = []
 
