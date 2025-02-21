ca785361199470a64c6a4e1ed2e28622c0db35c3
Evan Chime <evanchime@yahoo.co.uk>
2025-02-21 19:12:31 +0000

    Add entrypoint.sh

---
+++ me_website_project/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Database connection check
+if [[ $DATABASE_URL == postgres* ]]; then
+    until pg_isready -d $DATABASE_URL; do
+        echo "Waiting for PostgreSQL..."
+        sleep 2
+    done
+elif [[ $DATABASE_URL == mysql* ]]; then
+    HOST=$(echo $DATABASE_URL | awk -F'@' '{print $2}' | awk -F'/' '{print $1}')
+    until mysqladmin ping -h $HOST --silent; do
+        echo "Waiting for MySQL..."
+        sleep 2
+    done
+fi
+
+# Django setup
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+# Start server
+exec "$@"
