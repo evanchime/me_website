c6cc5d9d10306d14a95b6eb111b0f309c8875cd6
Evan Chime <evanchime@yahoo.co.uk>
2025-02-21 21:58:51 +0000

    In settings.py, import environ library and do away with dotenv. Load environment variable using the environ library

--- me_website_project/me_website_project/settings.py
+++ me_website_project/me_website_project/settings.py
@@ -11,14 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-# from dotenv import load_dotenv
 import os
 import environ
 
 env = environ.Env() # Load environment variables
 env.read_env() # Read .env file if it exists
 
-# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
