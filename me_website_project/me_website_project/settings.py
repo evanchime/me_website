cff06d2d377036031ce3e31fc113bd7197114312
Evan Chime <evanchime@yahoo.co.uk>
2025-02-21 22:06:30 +0000

    In settings.py, using environ library, set DEBUG to True in dev and to False in prod

--- me_website_project/me_website_project/settings.py
+++ me_website_project/me_website_project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('ME_WEBSITE_DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
+DEBUG = env.bool('DEBUG', default=False)
 
 
 ALLOWED_HOSTS = []
