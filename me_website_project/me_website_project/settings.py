0e2f6d1936573229df59baf5c92135e49db21d64
Evan Chime <evanchime@yahoo.co.uk>
2025-02-21 22:49:36 +0000

    In settings.py, if the DATABASE_URL environment variable is set, use django-environ to parse it into a Django database configuration dictionary. If not, fall back to the default SQLite database.

--- me_website_project/me_website_project/settings.py
+++ me_website_project/me_website_project/settings.py
@@ -94,10 +94,10 @@ WSGI_APPLICATION = 'me_website_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(
+        'DATABASE_URL',
+        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}'
+    )
 }
 
 
