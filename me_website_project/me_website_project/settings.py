195939000c6045bde0f4bf1973d46efa7508cd87
Evan Chime <evanchime@yahoo.co.uk>
2025-02-25 19:41:49 +0000

    In settings.py, use django.environ to set EMAIL_HOST_USER/EMAIL_HOST_PASSWORD variables to their respective environment variables

--- me_website_project/me_website_project/settings.py
+++ me_website_project/me_website_project/settings.py
@@ -156,6 +156,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
 # You might need an Google app password for security
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS') 
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD') 
