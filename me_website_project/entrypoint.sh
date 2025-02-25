172e844afbcdefb62adbacd5c2d329acc8b8f973
Evan Chime <evanchime@yahoo.co.uk>
2025-02-25 23:38:57 +0000

    Use bash instead of sh shell in the entrypoint.sh script

--- me_website_project/entrypoint.sh
+++ me_website_project/entrypoint.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # Database connection check
-if [[ $DATABASE_URL == postgres* ]]; then
+if [[ "$DATABASE_URL" == postgres* ]]; then
     until pg_isready -d $DATABASE_URL; do
         echo "Waiting for PostgreSQL..."
         sleep 2
     done
-elif [[ $DATABASE_URL == mysql* ]]; then
+elif [[ "$DATABASE_URL" == mysql* ]]; then
     HOST=$(echo $DATABASE_URL | awk -F'@' '{print $2}' | awk -F'/' '{print $1}')
     until mysqladmin ping -h $HOST --silent; do
         echo "Waiting for MySQL..."
