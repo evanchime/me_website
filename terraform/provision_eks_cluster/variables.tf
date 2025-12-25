f5c3a63ee62b0a55fa3bf493b26be0351a5713be
Evan Chime <evanchime@yahoo.co.uk>
2025-12-25 11:41:13 +0000

    refactor(terraform): Remove unused RDS variables
    Removes several variables from `variables.tf` that were used for connecting to a pre-existing, external RDS instance.
    
    The following variables have been deleted:
    - `existing_rds_instance_name`
    - `database_master_password`
    - `existing_rds_security_group_id`
    
    These are no longer required since the RDS instance is now fully provisioned and managed within this Terraform module, with its password handled automatically by Secrets Manager.

--- terraform/provision_eks_cluster/variables.tf
+++ terraform/provision_eks_cluster/variables.tf
@@ -13,23 +13,6 @@ variable "me_website_image" {
   default     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-west-2.amazonaws.com/me_website:latest"
 }
 
-variable "existing_rds_instance_name" {
-  description = "RDS instance name"
-  type        = string
-  default     = "me-website-database-instance"
-}
-
-variable "database_master_password" {
-  description = "Master password for the RDS instance"
-  type        = string
-  sensitive   = true
-}
-
-variable "existing_rds_security_group_id" {
-    description = "Security group ID of the existing RDS instance"
-    type        = string
-}
-
 variable "domain_name" {
   description = "The domain name for the website"
   type        = string
