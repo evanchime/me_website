4f52ea1e0474e2c600612902a0231349b17ece8f
Evan Chime <evanchime@yahoo.co.uk>
2026-01-29 07:20:14 +0000

    refactor: Remove redundant project attribute from remote state
    This commit removes the `project` attribute from the `terraform_remote_state` data source configurations in both the `app` and `platform` workspaces.
    
    The project is already defined in the `cloud` block within the main `terraform.tf` file for each workspace. Removing this redundant attribute simplifies the configuration and relies on the inherited project setting.

--- terraform/platform/remote_state.tf
+++ terraform/platform/remote_state.tf
@@ -3,7 +3,6 @@ data "terraform_remote_state" "me_website_k8s_network" {
 
   config = {
     organization = "DevOps_As_A_Way"
-    project = "k8s-migration"
     workspaces = {
       name = "k8s-migration-network"
     }
